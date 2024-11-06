222d230b512ad2c4012c8ade4af553c0d21c61cd
Aizensama117450 <akmalhabibmarzuki@gmail.com>
2024-11-06 17:49:08 +0700

    aa

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import tailwind from '@astrojs/tailwind';
 
 import playformCompress from '@playform/compress';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [tailwind(), playformCompress()]
 });
