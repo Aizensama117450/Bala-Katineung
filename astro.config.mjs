49dc73a983fd28b5103d985a83a2dd961d6a1fc6
gitcontrol <gorila7450@gmail.com>
2024-11-06 16:13:44 +0700

    terimakasih

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
+import tailwind from '@astrojs/tailwind';
+
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind(), playformCompress()]
+});
